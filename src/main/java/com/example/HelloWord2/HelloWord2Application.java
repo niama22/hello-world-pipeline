c95fdb4cf279b88e5bd825dc03c5550614fc1177
niama sakhr <niama.sakhr22@gmail.com>
2025-03-01 15:26:27 +0100

    niama_sakhr1234990shis99pp

--- src/main/java/com/example/HelloWord2/HelloWord2Application.java
+++ src/main/java/com/example/HelloWord2/HelloWord2Application.java
@@ -1,13 +1,17 @@
 package com.example.HelloWord2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWord2Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloWord2Application.class);
+
 	public static void main(String[] args) {
+		logger.info("🚀 Démarrage de l'application HelloWord2...");
 		SpringApplication.run(HelloWord2Application.class, args);
 	}
-
 }
